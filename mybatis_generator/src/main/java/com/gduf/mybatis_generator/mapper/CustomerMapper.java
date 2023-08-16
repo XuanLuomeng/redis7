c40259b6f4c60309cd9fa42f4ed3af0aadc64e9f
XuanLuomeng <1079716693@qq.com>
2023-08-16 18:02:18 +0800

    1.0.0

---
+++ mybatis_generator/src/main/java/com/gduf/mybatis_generator/mapper/CustomerMapper.java
@@ -0,0 +1,7 @@
+package com.gduf.mybatis_generator.mapper;
+
+import com.gduf.mybatis_generator.entities.Customer;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CustomerMapper extends Mapper<Customer> {
+}
